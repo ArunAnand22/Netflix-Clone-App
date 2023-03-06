892855fa75d6e5ef5a95e6104816707a3c7df719
Arun Anand <arunanand.tech@gmail.com>
2023-03-06 18:05:42 +0300

    Added remaining movie sections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import RowPoster from './Components/RowPoster/RowPoster';
-import {orginals,action} from './Components/Url'
+import {orginals,action,trending,comedy,horror,romance,documentaries} from './Components/Url'
 
 function App() {
   return (
@@ -11,7 +11,12 @@ function App() {
      <Navbar/>
      <Banner/>
      <RowPoster url={orginals} title='Netflix Orginals'/>
+     <RowPoster url={trending} title='Trending' isSmall/>
      <RowPoster url={action} title='Action' isSmall/>
+     <RowPoster url={comedy} title='Comedy' isSmall/>
+     <RowPoster url={horror} title='Horror' isSmall/>
+     <RowPoster url={romance} title='Romance' isSmall/>
+     <RowPoster url={documentaries} title='Documentaries' isSmall/>
     </div>
   );
 }
